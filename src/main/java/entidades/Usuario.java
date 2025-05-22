6c964a360946efcc87894f94c83de4c6a705029d
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2025-05-22 08:56:01 +0200

    add usuario entity

---
+++ src/main/java/entidades/Usuario.java
@@ -0,0 +1,120 @@
+package entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author alvaro
+ */
+@Entity
+@Table(name = "cliente")
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u"),
+    @NamedQuery(name = "Usuario.findByNombre", query = "SELECT u FROM Usuario u WHERE u.nomUsuario = :nomUsuario")
+})
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idUsuario")
+    private Integer idUsuario;
+
+    @Basic(optional = false)
+    @Column(name = "nomUsuario", unique = true)
+    private String nomUsuario;
+
+    @Basic(optional = false)
+    @Column(name = "passUsuario")
+    private String passUsuario;
+
+    // Constructores
+    public Usuario() {
+    }
+
+    public Usuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Usuario(Integer idUsuario, String nomUsuario, String passUsuario) {
+        this.idUsuario = idUsuario;
+        this.nomUsuario = nomUsuario;
+        this.passUsuario = passUsuario;
+    }
+
+    // Getters y Setters
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomUsuario() {
+        return nomUsuario;
+    }
+
+    public void setNomUsuario(String nomUsuario) {
+        this.nomUsuario = nomUsuario;
+    }
+
+    public String getPassUsuario() {
+        return passUsuario;
+    }
+
+    public void setPassUsuario(String passUsuario) {
+        this.passUsuario = passUsuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.idUsuario);
+        hash = 41 * hash + Objects.hashCode(this.nomUsuario);
+        hash = 41 * hash + Objects.hashCode(this.passUsuario);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.nomUsuario, other.nomUsuario)) {
+            return false;
+        }
+        if (!Objects.equals(this.passUsuario, other.passUsuario)) {
+            return false;
+        }
+        return Objects.equals(this.idUsuario, other.idUsuario);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "idUsuario=" + idUsuario + ", nomUsuario=" + nomUsuario + '}';
+    }
+}
