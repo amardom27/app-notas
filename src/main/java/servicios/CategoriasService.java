82c591bc8ffe7d05542831bc8ef03be4afdf2d27
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2025-05-25 13:59:00 +0200

    crud categories

--- src/main/java/servicios/CategoriasService.java
+++ src/main/java/servicios/CategoriasService.java
@@ -3,6 +3,8 @@ package servicios;
 import controladores.CategoriasController;
 import entidades.Categorias;
 import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -11,13 +13,50 @@ import java.util.ArrayList;
 public class CategoriasService {
 
     private static final CategoriasController cc = new CategoriasController();
-    
-     public static void insertarEjemplo() {
+
+    public static void agregarCategoria(Categorias cat) {
+        try {
+            cc.create(cat);
+            JOptionPane.showMessageDialog(null, "Categoria creada correctamente.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+        } catch (RuntimeException e) {
+            JOptionPane.showMessageDialog(null, "Error introduciendo la categoria.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static List<Categorias> obtenerCategorias() {
+        List<Categorias> categorias = cc.findAll();
+        return categorias;
+    }
+
+    public static void modificarCategoriaPorId(Categorias cat) {
+        try {
+            cc.update(cat);
+            JOptionPane.showMessageDialog(null, "Categoria modificada correctamente.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+        } catch (RuntimeException e) {
+            JOptionPane.showMessageDialog(null, "Error modificando la categoria.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static void borrarCategoriaPorId(Integer id) {
+        try {
+            cc.delete(id);
+            JOptionPane.showMessageDialog(null, "Categoria borrada correctamente.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+        } catch (RuntimeException e) {
+            JOptionPane.showMessageDialog(null, "Error borrando la categoria.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static void insertarEjemplo() {
         var lista = new ArrayList<Categorias>();
         lista.add(new Categorias("peliculas"));
         lista.add(new Categorias("programacion"));
-         for (Categorias categorias : lista) {
-             cc.create(categorias);
-         }
+        for (Categorias categorias : lista) {
+            cc.create(categorias);
+        }
+    }
+
+    public static void ejemploBuscarId() {
+        Categorias cat = cc.findById(2);
+        System.out.println(cat);
     }
 }
